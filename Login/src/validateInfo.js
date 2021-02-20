63e4b59218e4fbc547736f9fcf07a3c487e206bd
Tharusha_Peiris <61264958+Tharusha-Nethmini-Peiris@users.noreply.github.com>
2021-02-21 02:28:03 +0530

    Update validateInfo.js

--- Login/src/validateInfo.js
+++ Login/src/validateInfo.js
@@ -8,21 +8,13 @@ export default function validateInfo(values) {
     //   errors.name = 'Enter a valid name';
     // }
   
-    if (!values.email) {
-      errors.email = 'Email required';
-    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
-      errors.email = 'Email address is invalid';
-    }
+    
     if (!values.password) {
       errors.password = 'Password is required';
     } else if (values.password.length < 6) {
       errors.password = 'Password needs to be 6 characters or more';
     }
   
-    if (!values.password2) {
-      errors.password2 = 'Password is required';
-    } else if (values.password2 !== values.password) {
-      errors.password2 = 'Passwords do not match';
-    }
+ 
     return errors;
   }
