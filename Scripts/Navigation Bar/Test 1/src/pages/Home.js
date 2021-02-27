b004fa832b013c7f41416d2b5bdc6c51fbd0c76f
Tharusha_Peiris <61264958+Tharusha-Nethmini-Peiris@users.noreply.github.com>
2021-02-27 22:42:02 +0530

    Update Home.js
    Coding of the home page content

--- Scripts/Navigation Bar/Test 1/src/pages/Home.js
+++ Scripts/Navigation Bar/Test 1/src/pages/Home.js
@@ -1,11 +1,34 @@
 import React from 'react';
-
 function Home() {
-  return (
-    <div className='home'>
-      <h1>Home</h1>
-    </div>
-  );
-}
+return (
+<div className='home'>
 
+   <div className="content">
+     
+      <h1>Elastic Prediction System</h1><br/>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus tincidunt urna quis<br/> condimentum.
+      Vestibulum nec mi iaculis, sollicitudin risus sed, facilisis risus. Quisque sit amet tempor lorem, <br/>
+      at consectetur m
+      Proin eu mi sed lacus dictum rutrum ac in arcu. Proin imperdiet metus sit amet augue pulvinar, <br/>
+      et tempus neque accumsa
+      <br/>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut cursus tincidunt urna quis<br/> condimentum.
+      Vestibulum nec mi iaculis, sollicitudin risus sed, facilisis risus. Quisque sit amet tempor lorem, <br/>
+      at consectetur m
+      Proin eu mi sed lacus dictum rutrum ac in arcu. Proin imperdiet metus sit amet augue pulvinar, <br/>
+      et tempus neque accumsa
+      
+      <button className="btn">About Us</button> 
+      <button className="btn2">Help</button> 
+   </p>
+   
+   </div>
+   
+   <img className='form-img' src='img/b15.jpg' alt='home page' />
+   <img className='form-img2' src='img/p2.jpg' alt='home page' />
+   <img className='form-img3' src='img/w1.jpg' alt='home page' />
+   <img className='form-img4' src='img/bg.jpg' alt='home page' />
+ 
+</div>
+);
+}
 export default Home;
