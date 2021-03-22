095e0b3f4a75131d85fdf40d6e00ae0ff17e526d
Tharusha_Peiris <61264958+Tharusha-Nethmini-Peiris@users.noreply.github.com>
2021-03-22 22:02:37 +0530

    Add files via upload

---
+++ Home/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import User from './pages/User';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Products from './pages/Products';
+import Team from './pages/Team';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/user' component={User} />
+          
+          <Route path='/reports' component={Reports} />
+          <Route path='/products' component={Products} />
+          <Route path='/team' component={Team} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
+
+export default App;
