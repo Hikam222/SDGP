9f3af070d82c8d8d25fc8805422b2533bdcfe765
Tharusha_Peiris <61264958+Tharusha-Nethmini-Peiris@users.noreply.github.com>
2021-03-23 13:29:02 +0530

    Add files via upload

---
+++ home page + Report +Team all/src/pages/InputAdornments.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+// import IconButton from '@material-ui/core/IconButton';
+import Input from '@material-ui/core/Input';
+// import FilledInput from '@material-ui/core/FilledInput';
+// import OutlinedInput from '@material-ui/core/OutlinedInput';
+// import InputLabel from '@material-ui/core/InputLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+// import TextField from '@material-ui/core/TextField';
+// import Visibility from '@material-ui/icons/Visibility';
+// import VisibilityOff from '@material-ui/icons/VisibilityOff';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    margin: {
+      margin: theme.spacing(1),
+    },
+    withoutLabel: {
+      marginTop: theme.spacing(3),
+    },
+    textField: {
+      width: '25ch',
+    },
+  }),
+);
+
+interface State {
+  amount: string;
+  password: string;
+  weight: string;
+  weightRange: string;
+  showPassword: boolean;
+}
+
+export default function InputAdornments() {
+  const classes = useStyles();
+  const [values, setValues] = React.useState<State>({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop: keyof, State) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className={classes.root}>
+      <div>
+        
+        <FormControl className={clsx(classes.margin, classes.withoutLabel, classes.textField)}>
+          <Input
+            id="standard-adornment-weight"
+            value={values.weight}
+            onChange={handleChange('weight')}
+            endAdornment={<InputAdornment position="end">Kg</InputAdornment>}
+            aria-describedby="standard-weight-helper-text"
+            inputProps={{
+              'aria-label': 'weight',
+            }}
+          />
+          <FormHelperText id="standard-weight-helper-text">Weight</FormHelperText>
+        </FormControl>
+
+      </div>
+    </div>
+  );
+}
---
+++ home page + Report +Team all/src/pages/Team.js
@@ -0,0 +1,117 @@
+import React,{useState} from 'react';
+import './Team.css';
+
+
+function Team(){
+
+const [pname] = useState('M.B.M. Hikam');
+const [job] = useState('Undergraduate at University of Westminster');
+const [about] = useState('Someone who enjoys finding solutions to difficult problems.');
+const [p1name] = useState('Dumindu Hewarachchi');
+const [job1] = useState('Undergraduate at University of Westminster');
+const [about1] = useState('Someone who enjoys finding solutions to difficult problems.');
+const [pname2] = useState('Sayuru Gunathilaka');
+const [job2] = useState('Undergraduate at University of Westminster');
+const [about2] = useState('Someone who enjoys finding solutions to difficult problems.');
+const [pname3] = useState('Thamadie Gunawardana');
+const [job3] = useState('Undergraduate at University of Westminster');
+const [about3] = useState('Someone who enjoys finding solutions to difficult problems.');
+const [pname4] = useState('Jayami Rasara');
+const [job4] = useState('Undergraduate at University of Westminster');
+const [about4] = useState('Someone who enjoys finding solutions to difficult problems.');
+const [pname5] = useState('Tharusha Peiris');
+const [job5] = useState('Undergraduate at University of Westminster');
+const [about5] = useState('Someone who enjoys finding solutions to difficult problems.');
+
+
+return(
+    <div className='Team'>
+        <h1>Team</h1>
+
+        <div className="A-team-container">
+            
+            <div className="content">
+                <div className="upper-container">
+                    <div className="image-container">
+                    <img src="user/b3.jpg" alt="Team Leader" height="100px" width="100px"/>
+                    </div>
+                </div>
+            <div className="lower-container">
+                <h3>{ pname }</h3><br/>
+                <h4>{job}</h4>
+                <p>{ about }</p>
+                <button><a href="https://www.linkedin.com/in/mohamed-hikam-b94531198/">Visit Profile </a></button>
+            </div>
+            </div>
+            <div className="content">
+                <div className="upper-container">
+                    <div className="image-container">
+                    <img src="user/b3.jpg" alt="Team Leader" height="100px" width="100px"/>
+                    </div>
+                </div>
+            <div className="lower-container">
+                <h3>{ p1name }</h3><br/>
+                <h4>{job1}</h4>
+                <p>{ about1 }</p>
+                <button>Visit Profile</button>
+            </div>
+            </div>
+            <div className="content">
+                <div className="upper-container">
+                    <div className="image-container">
+                    <img src="user/b3.jpg" alt="Team Leader" height="100px" width="100px"/>
+                    </div>
+                </div>
+            <div className="lower-container">
+                <h3>{ pname2 }</h3><br/>
+                <h4>{job2}</h4>
+                <p>{ about2 }</p>
+                <button><a href="https://www.linkedin.com/in/sayuru-gunathilaka-6225131a6/">Visit Profile </a></button>
+            </div>
+            </div>
+            <div className="content">
+                <div className="upper-container">
+                    <div className="image-container">
+                    <img src="user/g3.jpg" alt="Team Leader" height="100px" width="100px"/>
+                    </div>
+                </div>
+            <div className="lower-container">
+                <h3>{ pname3 }</h3><br/>
+                <h4>{job3}</h4>
+                <p>{ about3 }</p>
+                <button><a href="https://www.linkedin.com/in/thamadiedulsandi/">Visit Profile </a></button>
+            </div>
+            </div>
+            <div className="content">
+                <div className="upper-container">
+                    <div className="image-container">
+                    <img src="user/g3.jpg" alt="Team Leader" height="100px" width="100px"/>
+                    </div>
+                </div>
+            <div className="lower-container">
+                <h3>{ pname4 }</h3><br/>
+                <h4>{job4}</h4>
+                <p>{ about4 }</p>
+                <button>Visit Profile</button>
+            </div>
+            </div>
+            <div className="content">
+                <div className="upper-container">
+                    <div className="image-container">
+                    <img src="user/g3.jpg" alt="Team Leader" height="100px" width="100px"/>
+                    </div>
+                </div>
+            <div className="lower-container">
+                <h3>{ pname5 }</h3><br/>
+                <h4>{job5}</h4>
+                <p>{ about5 }</p>
+                <button><a href="https://www.linkedin.com/in/tharusha-peiris-a07906195/">Visit Profile</a></button>
+            </div>
+            </div>
+        </div>
+        
+    </div>
+);
+}
+
+export default Team;
