97a07f34da2bd51f63a408ccc0896c67b7df5204
Thamadie <thamadie13@gmail.com>
2021-04-03 20:48:10 +0530

    update Flask_DB

--- Flask_DB/app.py
+++ Flask_DB/app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    request_method = request.method
+    return render_template('hello.html', request_method=request_method)
 
 
 if __name__ == '__main__':
--- Flask_DB/vars.py
+++ Flask_DB/vars.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'HI'
+    return render_template('hello.html')
 
 @app.route('/<string:name>')
 def greet(name):
