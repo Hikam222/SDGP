9827accfdf1489f7d2032406b3082a0b1368f60c
Thamadie <thamadie13@gmail.com>
2021-04-03 21:59:07 +0530

    update Flask_DB

--- Flask_DB/app.py
+++ Flask_DB/app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     request_method = request.method
+    if request_method == 'POST':
+        first_name = request.form['first_name']
+        return redirect(url_for('name',first_name=first_name))
     return render_template('hello.html', request_method=request_method)
 
+@app.route('/name/<string:first_name>')
+def name(first_name):
+    return f'{first_name}'
 
 if __name__ == '__main__':
     app.run(debug=True)
