e31a63c48b1063091e9f46fc2bfa48ba06257a29
Thamadie <thamadie13@gmail.com>
2021-04-19 20:08:36 +0530

    update Flask_Mongodb

--- Flask_Mongodb/app.py
+++ Flask_Mongodb/app.py
@@ -16,6 +16,16 @@ def redirect_url():
            request.referrer or \
            url_for('index')
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect("/")
+    return render_template('login.html', error=error)		   
+
 @app.route("/list")
 def lists ():
 	#Display the all Tasks
