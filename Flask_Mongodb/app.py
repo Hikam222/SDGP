02ecd41638ee24cac64d6daf8c09ceef3f266aaf
Thamadie <thamadie13@gmail.com>
2021-04-19 23:31:15 +0530

    Update Flask_Mongodb

--- Flask_Mongodb/app.py
+++ Flask_Mongodb/app.py
@@ -26,75 +26,12 @@ def login():
             return redirect("/")
     return render_template('login.html', error=error)		   
 
-@app.route("/list")
+@app.route("/")
 def lists ():
 	#Display the all Tasks
 	todos_l = todos.find()
 	a1="active"
 	return render_template('index.html',a1=a1,todos=todos_l,t=title,h=heading)
-
-@app.route("/")
-@app.route("/uncompleted")
-def tasks ():
-	#Display the Uncompleted Tasks
-	todos_l = todos.find({"done":"no"})
-	a2="active"
-	return render_template('index.html',a2=a2,todos=todos_l,t=title,h=heading)
-
-
-@app.route("/completed")
-def completed ():
-	#Display the Completed Tasks
-	todos_l = todos.find({"done":"yes"})
-	a3="active"
-	return render_template('index.html',a3=a3,todos=todos_l,t=title,h=heading)
-
-@app.route("/done")
-def done ():
-	#Done-or-not ICON
-	id=request.values.get("_id")
-	task=todos.find({"_id":ObjectId(id)})
-	if(task[0]["done"]=="yes"):
-		todos.update({"_id":ObjectId(id)}, {"$set": {"done":"no"}})
-	else:
-		todos.update({"_id":ObjectId(id)}, {"$set": {"done":"yes"}})
-	redir=redirect_url()	
-
-	return redirect(redir)
-
-@app.route("/action", methods=['POST'])
-def action ():
-	#Adding a Task
-	JobNo=request.values.get("JobNo")
-	ItemCode=request.values.get("ItemCode")
-	ColorGroup=request.values.get("ColorGroup")
-	waste_pecentage=request.values.get("waste_pecentage")
-	todos.insert({ "JobNo":JobNo, "ItemCode":ItemCode, "ColorGroup":ColorGroup, "waste_pecentage":waste_pecentage, "done":"no"})
-	return redirect("/list")
-
-@app.route("/remove")
-def remove ():
-	#Deleting a Task with various references
-	key=request.values.get("_id")
-	todos.remove({"_id":ObjectId(key)})
-	return redirect("/")
-
-@app.route("/update")
-def update ():
-	id=request.values.get("_id")
-	task=todos.find({"_id":ObjectId(id)})
-	return render_template('update.html',tasks=task,h=heading,t=title)
-
-@app.route("/action3", methods=['POST'])
-def action3 ():
-	#Updating a Task with various references
-	JobNo=request.values.get("JobNo")
-	ItemCode=request.values.get("ItemCode")
-	ColorGroup=request.values.get("ColorGroup")
-	waste_pecentage=request.values.get("waste_pecentage")
-	id=request.values.get("_id")
-	todos.update({"_id":ObjectId(id)}, {'$set':{ "JobNo":JobNo, "ItemCode":ItemCode, "ColorGroup":ColorGroup, "waste_pecentage":waste_pecentage }})
-	return redirect("/")
 
 @app.route("/search", methods=['GET'])
 def search():
