0d46c6ca72050fb5f077f107e0f27f5eb7678f7c
Thamadie <thamadie13@gmail.com>
2021-04-22 15:32:07 +0530

    update app.js

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,32 +1,32 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Side bar/Navbar';
+import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Reports from './pages/Reports/Reports';
-import Products from './pages/Products/Products';
 import Team from './pages/Team/Team';
 import User from './pages/User/User';
 import Form from './User login/Form';
 import './User login/UserLogin.css';
-import Messages from "./pages/Messages/Messages";
+import ProductComponent from './pages/Products/Product';
+import HeaderComponent from './pages/Products/HeaderComponent';
+import SearchComponent from './pages/Products/SearchComponent';
+
 
 function App() {
   return (
 
       <Router>
-
-        {/*<Form />*/}
-
-        {/*<Navbar/>*/}
+        <Form />
+        <Navbar/>
         <Switch>
-            <Route path='/' exact component={Form} />
-          <Route path='/home'  exact component={Home} />
+        
+          <Route path='/home' exact component={Home} />
           <Route path='/user' component={User} />
           <Route path='/reports' component={Reports} />
-          <Route path='/products' component={Products} />
+          <Route path = "/product" component = {ProductComponent}></Route>
+          <Route path = "/search" component = {SearchComponent}></Route>
           <Route path='/team' component={Team} />
-            <Route path='/messages' component={Messages} />
           
         </Switch>
       </Router>
