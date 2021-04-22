1211b7b3192f85ca365d8bcf59d7ca898bfdea9d
Thamadie <thamadie13@gmail.com>
2021-04-22 15:30:49 +0530

    update product

--- Front-end/src/pages/Products/Products.js
+++ Front-end/src/pages/Products/Products.js
@@ -1,16 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react'
 import Navbar from "../../Side bar/Navbar";
+import UserService from '../services/UserService'
 
-function Products() {
-  return (
-      <div>
-          <Navbar/>
-    <div className='products'>
-      <h1>Products</h1>
-    </div>
-</div>
-    
-  );
+class Product extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                elastic: []
+        }
+        
+    }
+
+    componentDidMount(){
+        UserService.getUsers().then((res) => {
+            this.setState({ elastic: res.data});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar/>
+                <div span="right" >
+                <form action="/search"	method="GET" >
+                    <table class="none" id="close">
+                    <tr>
+                    <td></td><td></td>
+                    <td><b>Search Reference:</b></td>
+                    <td><select name="refer" required>
+                        <option value="JobNo">JobNo</option>
+                        <option value="ItemCode">ItemCode</option>
+                        <option value="ColorGroup">ColorGroup</option>
+                        <option value="Total_Quantity">Total_Quantity</option>
+                        <option value="Total_Wastage">Total_Wastage</option>
+                    </select></td>
+                    <td><input type="text" name="key" placeholder="Search" size="15" /></td>
+                    <td><button type="submit">Search</button></td>
+                    </tr>
+                    </table>
+                </form>
+                </div>
+                 <h2 className="text-center">Elastic List</h2>
+                 <br></br>
+                 <div className = "row">
+                        <table className = "table table-striped table-bordered">
+
+                            <thead>
+                                <tr>
+                                    <th> JOBNO</th>
+                                    <th> ITEM CODE</th>
+                                    <th> COLOUR GROUP</th>
+                                    <th> TOTAL QUANTITY</th>
+                                    <th> TOTAL WASTAGE</th>
+                                    <th> WASTAGE PRECENTAGE</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.elastic.map(
+                                        elastic => 
+                                        <tr key = {elastic.id}>
+                                             <td> {elastic.JobNo} </td>   
+                                             <td> {elastic.ItemCode}</td>
+                                             <td> {elastic.ColorGroup}</td>
+                                             <td> {elastic.Total_Quantity}</td>
+                                             <td> {elastic.Total_Wastage}</td>
+                                             <td> {elastic.waste_pecentage}</td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </table>
+
+                 </div>
+
+            </div>
+        )
+    }
 }
 
-export default Products;
+export default Product
